62e4a7437cff992cce7cf9be6b469c9753305778
Quade Morrison <quade@dropbox.com>
2022-05-13 21:08:07 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:bankocr.go

--- bankocr.go
+++ bankocr.go
@@ -1,9 +1,44 @@
 package bankocr
 
+import "strings"
+
+// _  _     _  _  _  _  _
+// | _| _||_||_ |_   ||_||_|
+// ||_  _|  | _||_|  ||_| _|
+
+const (
+	ERROR = "ERR"
+	ILLEGAL = "ILL"
+	ZERO =
+		" _ " +
+		"| |" +
+		"|_|"
+)
+
+
 // Parse returns a numeric string for the input.
 func Parse(input string) string {
-	stringToNum := {
-
+	if len(input) != 27 {
+		return ILLEGAL
 	}
+
+	stringToNum := map[string]string{
+		ZERO: "0",
+	}
+	var result string
+
+	lines := strings.Split(input, "\n")
+	if len(lines) != 4 {
+		return ILLEGAL
+	}
+
+	var parsedNums [9]string
+
+	for line := range lines[:3] {
+		for i := 0; i < len(line); i += 3 {
+
+		}
+	}
+
 	return input
 }
